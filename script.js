a38da715fe248c6526d0b698bd77b3128a8d9520
Shaehyan <shaehyansamukti@gmail.com>
2025-03-26 14:30:23 +0700

    update beranda

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let eyeicon = document.getElementById("eyeicon");
 let password = document.getElementById("password");
 let konfirm = document.getElementById("konfirm")
 
+const carousel = document.getElementById("carousel");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
 eyeicon.onclick = function(){
     if(password.type == "password"){
         password.type = "text";
@@ -11,3 +15,11 @@ eyeicon.onclick = function(){
         eyeicon.src = "./src/visibility_off.svg";
     }
 }
+
+nextBtn.addEventListener("click", () => {
+    carousel.scrollBy({ left: 320, behavior: "smooth" });
+});
+
+prevBtn.addEventListener("click", () => {
+    carousel.scrollBy({ left: -320, behavior: "smooth" });
+});
